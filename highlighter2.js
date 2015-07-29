ce1ea09f8a5a0ca8f120ff559826647d0f8f87cb
Bryan MacDiarmid <brymac@gmail.com>
2015-07-29 18:09:22 -0400

    Comments and console.log cleaned up

--- highlighter2.js
+++ highlighter2.js
@@ -42,13 +42,10 @@ $(document).on('ready page:load', function() {
 
       if (sel.rangeCount && (range.toString().length > 0)) {
         // Create a new highlight
-        // var currentHighlightText = currentHighlightText.toString().trim().replace(/(\r\n|\n|\r)/g,"");
         var newHighlightText = range.toString().replace(/\s{2,}/g, ' ');
         newHighlightText = newHighlightText.replace(/\t/g, ' ');
         newHighlightText = newHighlightText.trim().replace(/(\r\n|\n|\r)/g,"");
-        // var newHighlightText = range.toString().replace(/\s\s+/g, ' ');
         var newHighlight = new Highlight(range.startContainer, range.startOffset, range.endContainer, range.endOffset, range.commonAncestorContainer, newHighlightText, range.startContainer.parentElement.id, getOccurences(range.startContainer.textContent, range.toString()), sel.focusOffset);
-        console.log(newHighlight)
         // Store highlight in array
         highlights.push(newHighlight);
         // Add a span to the range
@@ -95,78 +92,30 @@ function newHighlightSpan(id) {
   function buildHighlights() {
     // Make sure article is pristine
     deleteHighlights();
-    // Loop through the array of highlights backwards
+    // Loop through the array of highlights 
     var paragraphTextRanOnce = null;
     for (var i=0; i<(highlights.length); i++) {
-    // for (var i=(highlights.length-1); i>=(highlights.length-1); i--) {
-      //Using this for statement instead of the below solves the immediate problem of multiple highlights not loading if highlights ar erebuilt after the first highlight was made.  However, it may not work with the database.
-    // for (var i=(highlights.length-1); i>=0; i--) {
-      // Get the full text of the current highlight
       var currentHighlightText = highlights[i].text;
       var finalMarkedText = "";
 
       $('article.container p').each(function() {
-      // $('article').contents().filter(function() {
-        console.log("***************** this.id:")
-        console.log(this.id)
         if (this.id == highlights[i].parentElementID) {
           $this = $(this)
           if (paragraphTextRanOnce == null) {
             paragraphText = $this.html() 
             paragraphTextRanOnce = true;
           }
-          console.log("paragraphText:")
-          console.log(paragraphText)
-
-          // searchOccurences = getIndicesOf(currentHighlightText, (highlights[i].startContainer.textContent + highlights[i].text));  // This is checking the paragraph for occurences, but the searh index is based on the node.
 
           paragraphOccurences = getOccurences(paragraphText, currentHighlightText)
           replacementIndex = paragraphOccurences[highlights[i].occurenceIndex]
-          console.log("Highlight text length:");
-          console.log(highlights[i].text.length)
-          console.log("Line breaks:")
-          console.log((highlights[i].text.match(/\n/g)||[]).length)
-          replacementIndexEnd = replacementIndex + highlights[i].text.length; // + ((highlights[i].text.match(/\n/g)||[]).length);
-          console.log("replacement index:");
-          console.log(replacementIndex);
-          // console.log(paragraphText.substr(replacementIndex,replacementIndex));
-          console.log("replacement index end:");
-          console.log(replacementIndexEnd);
-          // console.log(paragraphText.substr(replacementIndexEnd,replacementIndexEnd));
-          console.log("First part of string:");
-          console.log(paragraphText.substr(0,replacementIndex));
+          replacementIndexEnd = replacementIndex + highlights[i].text.length;
           finalMarkedText = finalMarkedText.concat(paragraphText.substr(0,replacementIndex))
           finalMarkedText = finalMarkedText.concat("<span class='highlight'>");
-          // finalMarkedText = finalMarkedText.concat(paragraphText.substr(replacementIndex,replacementIndexEnd))
-          console.log("Text:")
-          console.dir(highlights[i].text)
           finalMarkedText = finalMarkedText.concat(highlights[i].text);
           finalMarkedText = finalMarkedText.concat("</span>");
-          console.log("Last part of string");
-          console.log(paragraphText.substr(replacementIndexEnd));
           finalMarkedText = finalMarkedText.concat(paragraphText.substr(replacementIndexEnd))
 
-          // occurenceIndex = searchOccurences[highlights[i].occurenceIndex]; 
-          // var finalMarkedText = ""
-          // var childLength =  highlights[i].startContainer.parentNode.childNodes.length;
-          // console.log("******************************************")
-          // console.log("About to loop for following highlight for highlight # " + i + ":");
-          // console.log(highlights[i].text);
-          // console.log(highlights[i]);
-          // for (x = 0; x < childLength; x++) {
-          //   console.log("About to concat the following node, node #" + x);
-          //   currentNode = highlights[i].startContainer.parentNode.childNodes[x]
-          //   console.log(currentNode.outerHTML || currentNode.textContent)
-
-          //   finalMarkedText = finalMarkedText.concat(currentNode.outerHTML || currentNode.textContent);
-          //   // If this doesn't work once we connect it to the database, we may need to do the following:
-          //   // Check if node is the current node in question, and if so modify the HTML string by using substr and the occurence index.
-          // }
-          // console.log("FInished product for final marked text:")
-          // console.log(finalMarkedText);
           paragraphText = finalMarkedText;
-
-          // $(this).html(finalMarkedText);
 
           $this.empty();
           $this.append(finalMarkedText);
